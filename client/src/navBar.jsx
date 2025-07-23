1e1df185751efbec7cf274b432ff97712b7223e8
Farzaan Demeri <farzaand@192.168.1.4>
2025-07-23 20:36:08 +0530

    Update-1

---
+++ client/src/navBar.jsx
@@ -0,0 +1,25 @@
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <nav>
+            <div className="navBar">
+                <ul className="navBarUL">
+                    <div className="navGroupLeft">
+                        <li className="navBarList">logo</li>
+                        <li className="navBarList"><button>Home</button></li>
+                        <li className="navBarList"><button>Price</button></li>
+                        <li className="navBarList"><button>Locations</button></li>
+                        <li className="navBarList"><button>Contact</button></li>
+                    </div>
+                    <div className="navGroupRight">
+                        <li className="navBarList"><button>Mode</button></li>
+                        <li className="navBarList signInButton"><button>Sign In</button></li>
+                    </div>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar
